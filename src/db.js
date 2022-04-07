d1768dc75b66562f6a596facb9e8818fa52e1c73
treepo1 <78393422+treepo1@users.noreply.github.com>
2022-04-07 15:43:24 -0300

    Add files via upload

--- src/db.js
+++ src/db.js
@@ -1,12 +1,16 @@
 const mysql = require('mysql');
+const MY_HOST = process.env.DB_HOST;
+const MY_USER = process.env.DB_USER;
+const MY_PASS =  process.env.DB_PASS;
+const MY_DB = process.env.DB_NAME;
+const MY_PORT = process.env.DB_PORT
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    insecureAuth: true,
-    port: process.env.DB_PORT
+    host: MY_HOST,
+    user: MY_USER,
+    password: MY_PASS,
+    database: MY_DB,
+    port: MY_PORT
 
 });
 
