86a475e5d2938226a30c070d89441040967c6d13
treepo1 <78393422+treepo1@users.noreply.github.com>
2022-04-07 08:20:52 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,23 @@
+require('dotenv').config({path:'.env'});
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+
+
+const routes = require('./routes');
+
+const server = express();
+
+const port = process.env.PORT || 3000
+
+server.use(cors());
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+
+server.use('/api', routes);
+
+server.listen(port, () => {
+    console.log(`Server running on: ${process.env.PORT}`)
+})
