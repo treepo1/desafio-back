49a8123fb4e544cc2cb3b0faa6d7ecfe9b767b77
treepo1 <78393422+treepo1@users.noreply.github.com>
2022-04-07 16:19:06 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const MY_HOST = process.env.DB_HOST;
 const MY_USER = process.env.DB_USER;
 const MY_PASS =  process.env.DB_PASS;
 const MY_DB = process.env.DB_NAME;
-const MY_PORT = process.env.PORT || 3000;
+const MY_PORT = process.env.DB_PORT;
 
 const connection = mysql.createConnection({
     host: MY_HOST,
